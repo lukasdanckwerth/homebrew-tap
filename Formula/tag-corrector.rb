07b7a82a14e8a15384b3fe2eea7ca2e15bd1d03a
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2019-08-09 23:43:33 +0200

    add tag-corrector formula

---
+++ Formula/tag-corrector.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class TagCorrector < Formula
+  desc "corrects ID3 tags"
+  homepage "https://gitlab.com/lukasdanckwerth/ID3Corrector"
+  sha256 "b4eae226f8dfca2c09e1f7e2763ddd240eb621b0efcbfaa682ed9ee45504dc50"
+  url "git@gitlab.com:lukasdanckwerth/ID3Corrector.git#releases", :using => :git, :tag => "0.0.1"
+
+  version "0.0.1"
+
+  def install
+    bin.install "bin/tag-corrector"
+  end
+
+  test do
+    system "false"
+  end
+end
