bc6fd3b829adc546585395f13f0f0ed883875559
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2019-08-10 01:28:18 +0200

    build tag-corrector via xcode build system

--- Formula/tag-corrector.rb
+++ Formula/tag-corrector.rb
@@ -1,15 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class TagCorrector < Formula
   desc "corrects ID3 tags"
   homepage "https://gitlab.com/lukasdanckwerth/ID3Corrector"
   sha256 "b4eae226f8dfca2c09e1f7e2763ddd240eb621b0efcbfaa682ed9ee45504dc50"
-  url "git@gitlab.com:lukasdanckwerth/ID3Corrector.git", :using => :git, :branch => "releases", :tag => "0.0.1"
+  url "git@gitlab.com:lukasdanckwerth/ID3Corrector.git", :using => :git, :tag => "0.0.3"
 
-  version "0.0.1"
+  version "0.0.3"
 
   def install
-    bin.install Dir["bin/*"]
+    if MacOS::Xcode.installed?
+      system "xcodebuild install"
+      system "ditto /tmp/ID3Corrector.dst/usr/local #{prefix}"
+    end
   end
 
   test do
