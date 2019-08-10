ac2c9493794c87adcc98bbfded7dfaaf4627248b
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2019-08-10 04:56:17 +0200

    update youtube-automator.rb: 0.0.2 => 0.0.6

--- Formula/youtube-automator.rb
+++ Formula/youtube-automator.rb
@@ -1,9 +1,9 @@
 class YoutubeAutomator < Formula
   desc "watches youtube for new videos"
   homepage "https://gitlab.com/lukasdanckwerth/youtube-automator"
-  url "git@gitlab.com:lukasdanckwerth/youtube-automator.git", :using => :git, :tag => "0.0.2"
+  url "git@gitlab.com:lukasdanckwerth/youtube-automator.git", :using => :git, :tag => "0.0.6"
 
-  version "0.0.2"
+  version "0.0.6"
 
   depends_on "youtube-dl"
   depends_on "jq"
