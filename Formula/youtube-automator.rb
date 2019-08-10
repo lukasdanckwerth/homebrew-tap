343d95af0adfacf2055834350c5c3ecf2d56914d
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2019-08-10 03:35:53 +0200

    add youtube-automator 0.0.2

--- Formula/youtube-automator.rb
+++ Formula/youtube-automator.rb
@@ -1,12 +1,13 @@
 class YoutubeAutomator < Formula
   desc "watches youtube for new videos"
   homepage "https://gitlab.com/lukasdanckwerth/youtube-automator"
-  url "git@gitlab.com:lukasdanckwerth/youtube-automator.git", :using => :git, :tag => "0.0.1"
+  url "git@gitlab.com:lukasdanckwerth/youtube-automator.git", :using => :git, :tag => "0.0.2"
 
-  version "0.0.1"
+  version "0.0.2"
 
   depends_on "youtube-dl"
   depends_on "jq"
+  depends_on "imagemagick"
 
   def install
     bin.install "youtube-automator"
