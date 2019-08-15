0051f956434eee8121dde196a481193a73bc1906
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2019-08-15 22:04:57 +0200

    add XcodeIconsUtil 0.0.3

--- Formula/XcodeIconsUtil.rb
+++ Formula/XcodeIconsUtil.rb
@@ -1,7 +1,7 @@
 class Xcodeiconsutil < Formula
-  desc "corrects ID3 tags"
+  desc "Easy create image sets."
   homepage "https://gitlab.com/lukasdanckwerth/XcodeIconsUtil"
-  url "git@gitlab.com:lukasdanckwerth/XcodeIconsUtil.git", :using => :git, :tag => "0.0.2"
+  url "git@gitlab.com:lukasdanckwerth/XcodeIconsUtil.git", :using => :git, :tag => "0.0.3"
 
   version "0.0.3"
 
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 - tag-corrector (corrects ID3 tags)
 - youtube-automator (watches youtube for new videos)
-
+- Xcodeiconsutil (easy create image sets)
