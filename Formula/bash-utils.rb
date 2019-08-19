b5941f029705669e71139005cc3856a65f535034
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2019-08-19 02:20:56 +0200

    update bash-utils.rb: 0.1.0 => 0.1.1

--- Formula/bash-utils.rb
+++ Formula/bash-utils.rb
@@ -1,15 +1,16 @@
 class BashUtils < Formula
   desc "useful terminal utils"
   homepage "https://gitlab.com/lukasdanckwerth/bash-utils"
-  url "git@gitlab.com:lukasdanckwerth/bash-utils.git", :using => :git, :tag => "0.1.0"
+  url "git@gitlab.com:lukasdanckwerth/bash-utils.git", :using => :git, :tag => "0.1.1"
 
-  version "0.1.0"
+  version "0.1.1"
 
   def install
     bin.install "gitcommitfast"
     bin.install "gitversiontag"
     bin.install "podupdatefast"
-    bin.install "updateformula"
+    bin.install "formulaupdate"
+    bin.install "formulaedit"
   end
 
   test do
