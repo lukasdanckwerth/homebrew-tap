e35feda39dea2b151418cb5fc052706d63d0ca2c
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2019-08-23 07:11:45 +0200

    update tag-corrector.rb: 0.0.3 => 0.0.4

--- Formula/tag-corrector.rb
+++ Formula/tag-corrector.rb
@@ -1,10 +1,9 @@
 class TagCorrector < Formula
   desc "corrects ID3 tags"
   homepage "https://gitlab.com/lukasdanckwerth/ID3Corrector"
-  sha256 "b4eae226f8dfca2c09e1f7e2763ddd240eb621b0efcbfaa682ed9ee45504dc50"
-  url "git@gitlab.com:lukasdanckwerth/ID3Corrector.git", :using => :git, :tag => "0.0.3"
+  url "git@gitlab.com:lukasdanckwerth/ID3Corrector.git", :using => :git, :tag => "0.0.4"
 
-  version "0.0.3"
+  version "0.0.4"
 
   def install
     if MacOS::Xcode.installed?
